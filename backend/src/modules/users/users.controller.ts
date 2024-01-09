a71af3b0613b780460c89cc77f2dc336e75d5157
Bartosz Suski <bialywilk50@gmail.com>
2024-01-10 00:07:37 +0100

    refactor: change prisma.model to global, change id to uuid, refactored types in users

--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { User as UserPrismaModel } from '.prisma/client';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/CreateUser.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(@Inject(UsersService) private usersService: UsersService) {}
+
+  @ApiTags('Users')
+  @Post()
+  async createUser(@Body() userData: CreateUserDto): Promise<UserPrismaModel> {
+    return this.usersService.createUser(userData);
+  }
 }
--- backend/src/modules/users/users.service.ts
+++ backend/src/modules/users/users.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User as UserPrismaModel } from '@prisma/client';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: CreateUserDto): Promise<UserPrismaModel> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+}
