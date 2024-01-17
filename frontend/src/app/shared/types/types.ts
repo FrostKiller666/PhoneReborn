f1ac02f0fe92ffbe3f9dd5b05aaa8997d16cc7d6
Bartosz Suski <bialywilk50@gmail.com>
2024-01-17 23:30:45 +0100

    chore: added preline feat: added tested menu in dashboard

---
+++ frontend/src/app/shared/types/types.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface CreateUserDto {
+  email: string;
+  name: string;
+}
+
+export interface ResUserDto {
+  statusCode: number;
+  message: string;
+}
